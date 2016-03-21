87f826104744c2776bd7b585bad4db2a2bc2ce6b
Felipe Apostol <flipjs.io@gmail.com>
2016-03-21 08:25:24 +0000

    update test and fix capitalize function

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ function isEmpty (obj) {
 }
 
 function capitalize (string) {
+  if (!string) return ''
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
--- test/index.test.js
+++ test/index.test.js
@@ -14,4 +14,15 @@ describe('flipjs', function () {
       expect(flipjs.isEmpty('not empty')).to.be.false
     })
   })
+  describe('capitalize', function () {
+    it('should capitalize a word', function () {
+      expect(flipjs.capitalize('chris')).to.be.equal('Chris')
+    })
+    it(
+      'should return an empty string when no params passed to it',
+      function () {
+        expect(flipjs.capitalize()).to.be.equal('')
+      }
+    )
+  })
 })
